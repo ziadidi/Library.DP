25a849273943d6eb18fd93ccdebb7617d392adb4
ziadidi <dianepucci19@gmail.com>
2022-09-15 17:04:26 +0100

    end

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -46,17 +46,17 @@ const Nav = ({numberOfItem}) => {
           </button>
           <ul className="menu__links">
             <li className="menu__list">
-              <Link to="/" className="menu__link">
+              <Link to="/" className="menu__link" onClick={closeMenu}>
                 Home
               </Link>
             </li>
             <li className="menu__list">
-              <Link to="/books" className="menu__link">
+              <Link to="/books" className="menu__link" onClick={closeMenu}>
                 Books
               </Link>
             </li>
             <li className="menu__list">
-              <Link to="/cart" className="menu__link">
+              <Link to="/cart" className="menu__link" onClick={closeMenu}>
                 Cart
               </Link>
             </li>
